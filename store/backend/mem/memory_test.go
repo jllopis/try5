4011cb51d9ccc63f641ae929eec011399a126e91
Joan Llopis <jllopisg@gmail.com>
2015-05-18 12:32:28 +0200

    fix account struct name

--- store/backend/mem/memory_test.go
+++ store/backend/mem/memory_test.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"testing"
 
-	"github.com/jllopis/try5/user"
+	"github.com/jllopis/try5/account"
 )
 
 func TestAccount(t *testing.T) {
-	user, err := user.NewUser("testuser@dom.local", "Test User", "SuperDifficultPass")
+	account, err := account.NewAccount("testaccount@dom.local", "Test account", "SuperDifficultPass")
 	if err != nil {
 		t.Fatal("Error creating account: ", err)
 	}
@@ -18,21 +18,21 @@ func TestAccount(t *testing.T) {
 		t.Fatal("Error creating mem store")
 	}
 
-	savedUser, err := m.SaveUser(user)
+	savedAccount, err := m.SaveAccount(account)
 	if err != nil {
-		t.Fatal("Error saving user to mem store:", err)
+		t.Fatal("Error saving account to mem store:", err)
 	}
-	fmt.Printf("Saved user: %#v\n", savedUser)
+	fmt.Printf("Saved account: %#v\n", savedAccount)
 
-	u := m.LoadUser(savedUser.UID)
+	u := m.LoadAccount(savedAccount.UID)
 	if u == nil {
-		t.Fatal("Error getting user from mem store")
+		t.Fatal("Error getting account from mem store")
 	}
 	fmt.Printf("Got from store: %#v\n", u)
 
-	u2 := m.LoadUser("")
+	u2 := m.LoadAccount("")
 	if u2 != nil {
-		t.Fatal("Got inexistent user from mem store")
+		t.Fatal("Got inexistent account from mem store")
 		fmt.Printf("Got from store: %#v\n", u2)
 	}
 
