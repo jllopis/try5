cee6effa66598b155449be9a8e6611538ff0d5d5
Joan Llopis <jllopisg@gmail.com>
2015-05-22 12:33:43 +0200

    updated deps

--- Godeps/_workspace/src/github.com/jllopis/aloja/mw/cors.go
+++ Godeps/_workspace/src/github.com/jllopis/aloja/mw/cors.go
@@ -1,20 +1,16 @@
 package mw
 
-import "net/http"
+import (
+	"net/http"
 
-func CorsHandler(h http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Allow cross domain Ajax requests
-		//if r.Method == "OPTIONS" {
-		if origin := r.Header.Get("Origin"); origin != "" {
-			w.Header().Set("Access-Control-Allow-Origin", origin)
-		}
-		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, PATCH, DELETE")
-		w.Header().Set("Access-Control-Request-Method", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Origin, X-Requested-With, Accept, Authorization")
-		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		//}
-		//w.Header().Set("Access-Control-Allow-Origin", "*")
-		h.ServeHTTP(w, r)
-	})
+	"github.com/rs/cors"
+)
+
+type CorsOptions cors.Options
+
+func CorsHandler(opts CorsOptions) Middleware {
+	return func(h http.Handler) http.Handler {
+		c := cors.New((cors.Options)(opts))
+		return c.Handler(h)
+	}
 }
