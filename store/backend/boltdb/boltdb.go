b685abc8398abc00f178760e4d06b15522d74dd3
Joan Llopis <jllopisg@gmail.com>
2015-05-22 18:51:52 +0200

    save account behavior modified
    When a request does not provide a UID, we treat as a new item. If it
    provides a UID then we should check if the account really exist, so we
    can update, or it does not and we return nil and an error.
    
    We also take care of the values that can not be updated: uid, created so
    we always have the rigth value.
    
    Also added logxi logger.

--- store/backend/boltdb/boltdb.go
+++ store/backend/boltdb/boltdb.go
@@ -5,7 +5,6 @@ import (
 	"encoding/gob"
 	"errors"
 	"fmt"
-	"log"
 	"time"
 
 	"code.google.com/p/go-uuid/uuid"
@@ -13,12 +12,14 @@ import (
 	"github.com/boltdb/bolt"
 	"github.com/jllopis/try5/account"
 	"github.com/jllopis/try5/store"
+	"github.com/mgutz/logxi/v1"
 )
 
 type BoltStore struct {
 	C      *bolt.DB
 	status int
 	BoltStoreOptions
+	logger log.Logger
 }
 
 type BoltStoreOptions struct {
@@ -27,9 +28,11 @@ type BoltStoreOptions struct {
 }
 
 func NewBoltStore(options *BoltStoreOptions) *BoltStore {
+	b := &BoltStore{logger: log.New("bolt")}
+	b.BoltStoreOptions = *options
 	db, err := bolt.Open(options.Dbpath, 0600, &bolt.Options{Timeout: options.Timeout * time.Second})
 	if err != nil {
-		log.Fatal(err)
+		b.logger.Fatal("NewBoltStore", "error", err.Error())
 	}
 	err = db.Update(func(tx *bolt.Tx) error {
 		_, err := tx.CreateBucketIfNotExists([]byte("accounts"))
@@ -45,8 +48,8 @@ func NewBoltStore(options *BoltStoreOptions) *BoltStore {
 	if err != nil {
 		return nil
 	}
-	b := &BoltStore{C: db, status: store.CONNECTED}
-	b.BoltStoreOptions = *options
+	b.C = db
+	b.status = store.CONNECTED
 	return b
 }
 
@@ -82,7 +85,7 @@ func (s *BoltStore) LoadAccount(uuid string) (*account.Account, error) {
 	err := s.C.View(func(tx *bolt.Tx) error {
 		data := tx.Bucket([]byte("accounts")).Get([]byte(uuid))
 		if data == nil {
-			return nil
+			return errors.New("account not found")
 		}
 		dec := gob.NewDecoder(bytes.NewBuffer(data))
 		return dec.Decode(&a)
@@ -120,30 +123,71 @@ func (s *BoltStore) GetAccountByEmail(email string) (*account.Account, error) {
 	return nil, errors.New("email not found")
 }
 
-func (s *BoltStore) SaveAccount(account *account.Account) (*account.Account, error) {
+func (s *BoltStore) SaveAccount(acc *account.Account) (*account.Account, error) {
 	now := time.Now().UTC()
-	account.Updated = &now
-	if account.Password != nil {
-		account.UpdatePassword(*account.Password)
-	}
-	if account.UID == nil {
+	acc.Updated = &now
+	// Check if we have an id. If we do, it "could" be an update (check if account exist first)
+	// If don't, its a new account
+	if acc.UID == nil {
 		u := uuid.New()
-		account.UID = &u
-		account.Created = &now
+		acc.UID = &u
+		acc.Created = &now
+		if ok := acc.Password; ok == nil {
+			return nil, errors.New("nil password")
+		}
+		acc.UpdatePassword(*acc.Password)
+		if acc.Active == nil {
+			t := true
+			acc.Active = &t
+		}
+	} else {
+		savedAcc, err := s.LoadAccount(*acc.UID)
+		if err != nil {
+			return nil, err
+		}
+		if savedAcc == nil {
+			s.logger.Info("SaveAccount", "cant retrieve account from db", "uid", *acc.UID)
+			return nil, errors.New("uid mismatch")
+		}
+		// copy immutable data, that we are not allowed to modify
+		acc.Created = savedAcc.Created
+		if acc.Password != nil {
+			if savedAcc.Password != nil {
+				s.logger.Info("SaveAccount", "change passord required", *acc.UID, "old", *savedAcc.Password, "new", *acc.Password)
+				if *acc.Password != *savedAcc.Password {
+					acc.UpdatePassword(*acc.Password)
+					s.logger.Info("SaveAccount", "passord changed", *acc.UID, "old", *savedAcc.Password, "new", *acc.Password)
+				}
+			}
+		} else {
+			if savedAcc.Password != nil {
+				acc.Password = savedAcc.Password
+			}
+		}
+		if acc.Active == nil {
+			if savedAcc.Active != nil {
+				acc.Active = savedAcc.Active
+			} else {
+				// active is true by default
+				t := true
+				acc.Active = &t
+			}
+		}
 	}
 	var buf bytes.Buffer
 	enc := gob.NewEncoder(&buf)
-	err := enc.Encode(account)
+	err := enc.Encode(acc)
 	if err != nil {
 		return nil, err
 	}
 	err = s.C.Update(func(tx *bolt.Tx) error {
-		return tx.Bucket([]byte("accounts")).Put([]byte(*account.UID), buf.Bytes())
+		return tx.Bucket([]byte("accounts")).Put([]byte(*acc.UID), buf.Bytes())
 	})
 	if err != nil {
 		return nil, err
 	}
-	return account, nil
+
+	return acc, nil
 }
 
 func (s *BoltStore) DeleteAccount(uuid string) (int, error) {
