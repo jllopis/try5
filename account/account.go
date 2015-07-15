909f9d46bac7c1941892b29b5f31c2f140ad4bd9
Joan Llopis <jllopisg@gmail.com>
2015-07-15 12:48:07 +0200

    account: added comments

--- account/account.go
+++ account/account.go
@@ -8,6 +8,8 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+// Account hold the information of an Account type. Variables are of type pointer
+// to easily identify null variables when persist/read to/from database storage.
 type Account struct {
 	ID       *int64     `json:"-" db:"id"`
 	UID      *string    `json:"uid" db:"uid"`
@@ -22,15 +24,29 @@ type Account struct {
 }
 
 var (
-	ErrInvalidName     = errors.New("invalid name")
+	// ErrInvalidName notifies than the name is not valid
+	ErrInvalidName = errors.New("invalid name")
+	// ErrInvalidPassword alert about an invalid password
 	ErrInvalidPassword = errors.New("invalid password")
-	ErrInvalidEmail    = errors.New("invalid email address")
+	// ErrInvalidEmail notifies than the provided email is no valid
+	ErrInvalidEmail = errors.New("invalid email address")
 
+	// GravatarURI is the URI of the gravatar service to show the user gravatar
 	GravatarURI = "https://gravatar.com/avatar/%s?s=%v"
-
+	// RegexpEmail check that the email is compatible with an email address
+	// TODO(jllopis): in go v1.5 ckeck if is of use net/mail.AddressParser.Parse()
 	RegexpEmail = regexp.MustCompile(`^[^@]+@[^@.]+\.[^@.]+`)
 )
 
+/*
+NewAccount creates a new instance of Account with the given email, name and
+password.
+
+email must be unique and can not exist two accounts with same email. However, the
+existance check will not be done until Save account into the store.
+
+If an error is found, nil is returned with the error.
+*/
 func NewAccount(email, name, password string) (*Account, error) {
 	account := &Account{Email: &email, Name: &name}
 	err := account.hashPassword([]byte(password))
@@ -40,25 +56,20 @@ func NewAccount(email, name, password string) (*Account, error) {
 	return account, nil
 }
 
+// SetPassword add the given password to the account. First it validates that
+// the password length is correct and then hash it with bcrypt.
 func (account *Account) SetPassword(password string) error {
 	if len(password) < 8 || len(password) > 256 {
 		return ErrInvalidPassword
 	}
-
-	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	err := account.hashPassword([]byte(password))
 	if err != nil {
 		return err
 	}
-	if account.Password == nil {
-		s := string(hash)
-		account.Password = &s
-		return nil
-	}
-	*account.Password = string(hash)
 	return nil
 }
 
-// Tiene que devolver nil, sino es que hay error
+// hashPassword do the bcrypt hashing and store it to the account
 func (account *Account) hashPassword(password []byte) error {
 	pass, err := bcrypt.GenerateFromPassword(password, 0)
 	if err != nil {
@@ -73,6 +84,7 @@ func (account *Account) hashPassword(password []byte) error {
 	return nil
 }
 
+// MatchPassword check if the given password match with the hash stored in the account.
 func (account *Account) MatchPassword(password string) error {
 	if err := bcrypt.CompareHashAndPassword([]byte(*account.Password), []byte(password)); err != nil {
 		return err
@@ -80,6 +92,7 @@ func (account *Account) MatchPassword(password string) error {
 	return nil
 }
 
+// UpdatePassword updates the hashed password in the account with the new string.
 func (account *Account) UpdatePassword(newPassword string) error {
 	err := account.hashPassword([]byte(newPassword))
 	if err != nil {
@@ -89,11 +102,14 @@ func (account *Account) UpdatePassword(newPassword string) error {
 	return nil
 }
 
+// DeletePassword erase the hash from the account. Afterwards the account will
+// have no password so it can not authenticate.
 func (account *Account) DeletePassword() error {
 	account.Password = nil
 	return nil
 }
 
+// Delete marks the account as deleted so it can not be used.
 func (account *Account) Delete() error {
 	if account.Deleted == nil {
 		d := true
@@ -104,15 +120,25 @@ func (account *Account) Delete() error {
 	return nil
 }
 
-func (a *Account) ValidateFields() error {
+/*
+ValidateFields make sure that the account fields match the requirements.
+
+The checks performed are:
+
+- Name: must exist and be of length between 1 and 256
+
+- Email: must exist and be a valid email address. Valid email should match regexp:
+  `^[^@]+@[^@.]+\.[^@.]+`
+*/
+func (account *Account) ValidateFields() error {
 	switch {
-	case a.Name == nil:
+	case account.Name == nil:
 		return ErrInvalidName
-	case a.Email == nil:
+	case account.Email == nil:
 		return ErrInvalidEmail
-	case len(*a.Name) == 0 || len(*a.Name) > 256:
+	case len(*account.Name) == 0 || len(*account.Name) > 256:
 		return ErrInvalidName
-	case len(*a.Email) == 0 || len(*a.Email) > 256 || RegexpEmail.MatchString(*a.Email) == false:
+	case len(*account.Email) == 0 || len(*account.Email) > 256 || RegexpEmail.MatchString(*account.Email) == false:
 		return ErrInvalidEmail
 	default:
 		return nil
