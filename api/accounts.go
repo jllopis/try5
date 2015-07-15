a496a62a1e97127a83c772942039ecc0088ed088
Joan Llopis <jllopisg@gmail.com>
2015-07-15 12:49:31 +0200

    api/accounts: keys and jwt work
    - added keypair generation on account creation
    - implemented methods GetAccountKeys and GetAccountTokens

--- api/accounts.go
+++ api/accounts.go
@@ -7,12 +7,13 @@ import (
 
 	"github.com/jllopis/aloja"
 	"github.com/jllopis/try5/account"
+	"github.com/jllopis/try5/keys"
 	"github.com/jllopis/try5/store"
 	"github.com/lib/pq"
 )
 
 // GetAllAccounts devuelve una lista con todos los accounts de la base de datos
-// curl -ks https://b2d:8000/v1/accounts | jp -
+//   curl -ks https://b2d:8000/v1/accounts | jp -
 func (ctx *ApiContext) GetAllAccounts(w http.ResponseWriter, r *http.Request) {
 	var res []*account.Account
 	var err error
@@ -24,7 +25,7 @@ func (ctx *ApiContext) GetAllAccounts(w http.ResponseWriter, r *http.Request) {
 }
 
 // GetAccountByID devuelve el account de la base de datos que coincide con el ID suministrado
-// curl -ks https://b2d:8000/v1/accounts/342947fd-6c4b-4d2b-85ab-da14b37d047a | jp -
+//   curl -ks https://b2d:8000/v1/accounts/342947fd-6c4b-4d2b-85ab-da14b37d047a | jp -
 func (ctx *ApiContext) GetAccountByID(w http.ResponseWriter, r *http.Request) {
 	var res *account.Account
 	var err error
@@ -42,7 +43,7 @@ func (ctx *ApiContext) GetAccountByID(w http.ResponseWriter, r *http.Request) {
 }
 
 // NewAccount crea un nuevo account.
-// curl -k https://b2d:8000/v1/accounts -X POST -d '{"email":"tu2@test.com","name":"test user 2","password":"1234","active":true}'
+//   curl -k https://b2d:8000/v1/accounts -X POST -d '{"email":"tu2@test.com","name":"test user 2","password":"1234","active":true}'
 func (ctx *ApiContext) NewAccount(w http.ResponseWriter, r *http.Request) {
 	var data account.Account
 	err := json.NewDecoder(r.Body).Decode(&data)
@@ -66,12 +67,22 @@ func (ctx *ApiContext) NewAccount(w http.ResponseWriter, r *http.Request) {
 		logger.Error("func NewAccount", "error", err)
 		return
 	} else {
+		// Generate and assign a key pair
+		if creat := r.FormValue("wkey"); creat == "" {
+			k := keys.New(*outdata.UID)
+			if _, err := ctx.DB.SaveKey(k); err != nil {
+				ctx.Render.JSON(w, http.StatusInternalServerError, &logMessage{Status: "error", Action: "create", Info: err.Error(), Table: "keys", Code: "500"})
+				logger.Error("func NewAccount/keys.New", "error", err)
+				return
+			}
+		}
+		// and return the created account
 		ctx.Render.JSON(w, http.StatusCreated, outdata)
 	}
 }
 
 // UpdateAccount actualiza los datos del account y devuelve el objeto actualizado.
-// curl -ks https://b2d:8000/v1/accounts/342947fd-6c4b-4d2b-85ab-da14b37d047a -X PUT -d '{}' | jp -
+//   curl -ks https://b2d:8000/v1/accounts/342947fd-6c4b-4d2b-85ab-da14b37d047a -X PUT -d '{}' | jp -
 func (ctx *ApiContext) UpdateAccount(w http.ResponseWriter, r *http.Request) {
 	var newdata account.Account
 	var err error
@@ -120,7 +131,7 @@ func (ctx *ApiContext) UpdateAccount(w http.ResponseWriter, r *http.Request) {
 }
 
 // DeleteAccount elimina el account solicitado.
-// curl -ks https://b2d:8000/v1/accounts/3 -X DELETE | jp -
+//   curl -ks https://b2d:8000/v1/accounts/3 -X DELETE | jp -
 func (ctx *ApiContext) DeleteAccount(w http.ResponseWriter, r *http.Request) {
 	var uid string
 	if uid = aloja.Params(r).ByName("uid"); uid == "" {
@@ -142,4 +153,38 @@ func (ctx *ApiContext) DeleteAccount(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
+}
+
+// GetAccountKeys obtiene la pareja de claves RSA pública y privada de la cuenta
+//   curl -ksi https://b2d:8000/v1/accounts/3/keys | jp -
+func (ctx *ApiContext) GetAccountKeys(w http.ResponseWriter, r *http.Request) {
+	var uid string
+	if uid = aloja.Params(r).ByName("uid"); uid == "" {
+		ctx.Render.JSON(w, http.StatusInternalServerError, &logMessage{Status: "error", Action: "get", Info: "uid cannot be nil"})
+		return
+	}
+	var res *keys.Key
+	var err error
+	if res, err = ctx.DB.GetKeyByAccountID(uid); err != nil {
+		ctx.Render.JSON(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	ctx.Render.JSON(w, http.StatusOK, res)
+}
+
+// GetAccountTokens obtiene la pareja de claves RSA pública y privada de la cuenta
+//   curl -ksi https://b2d:8000/v1/accounts/3/tokens | jp -
+func (ctx *ApiContext) GetAccountTokens(w http.ResponseWriter, r *http.Request) {
+	var uid string
+	if uid = aloja.Params(r).ByName("uid"); uid == "" {
+		ctx.Render.JSON(w, http.StatusInternalServerError, &logMessage{Status: "error", Action: "get", Info: "uid cannot be nil"})
+		return
+	}
+	var res string
+	var err error
+	if res, err = ctx.DB.GetTokenByAccountID(uid); err != nil {
+		ctx.Render.JSON(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	ctx.Render.JSON(w, http.StatusOK, res)
 }
