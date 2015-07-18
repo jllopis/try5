1b5de2d41f3a987c1d79c31740324ca8969eea1f
Joan Llopis <jllopisg@gmail.com>
2015-07-18 18:34:26 +0200

    try5d: moved to Echo

--- cmd/try5d/main.go
+++ cmd/try5d/main.go
@@ -10,13 +10,13 @@ import (
 	"time"
 
 	"bitbucket.org/jllopis/getconf"
-	"github.com/gorilla/securecookie"
-	"github.com/jllopis/aloja"
-	"github.com/jllopis/aloja/mw"
 	"github.com/jllopis/try5/api"
 	logger "github.com/jllopis/try5/log"
-	"github.com/jllopis/try5/store/backend/boltdb"
-	"github.com/unrolled/render"
+	_ "github.com/jllopis/try5/store/backend/boltdb"
+	"github.com/jllopis/try5/store/manager"
+	"github.com/labstack/echo"
+	mw "github.com/labstack/echo/middleware"
+	"github.com/rs/cors"
 )
 
 // Config proporciona la configuración del servicio para ser utilizado por getconf
@@ -41,10 +41,11 @@ var (
 	// Version holds the version number of the build. It is valued at compile time
 	Version string
 	// Revision holds the git revision of the binary. It is valued at compile time
-	Revision string
-	config   *getconf.GetConf
-	apiCtx   *api.ApiContext
-	verbose  bool
+	Revision    string
+	config      *getconf.GetConf
+	mainManager *manager.Manager
+	//	apiCtx   *api.ApiContext
+	verbose bool
 )
 
 func init() {
@@ -52,46 +53,41 @@ func init() {
 	//config = getconf.New(&Config{}, "TRY5", true, etcdURI)
 	config = getconf.New(&Config{}, "TRY5", false, "")
 	config.Parse()
-	//	dbPort := 5432
-	//	if p, err := config.GetInt("StorePort"); err == nil {
-	//		logger.LogI("Store", "port", p)
-	//		dbPort = int(p)
-	//	}
+	//dbPort := 5432
+	//if p, err := config.GetInt("StorePort"); err == nil {
+	//dbPort = int(p)
+	//}
+	//logger.LogD("Store", "port", dbPort)
 	//	rs, err := psql.OpenPgSQLStore(&psql.PsqlStoreOptions{
 	//		Host:     config.GetString("StoreHost"),
 	//		Port:     dbPort,
 	//		DBName:   config.GetString("StoreName"),
-	//		User:     config.GetString("StoreUser"),
+	//		User:     config.GetString("StoreUser",)
 	//		Password: config.GetString("StorePass"),
 	//	})
-	timeout := 5 * time.Second
-	if to, err := config.GetInt("StoreTimeout"); err == nil {
-		logger.LogI("Store", "connect timeout (s)", to)
-		timeout = time.Duration(to) * time.Second
-	}
-	rs := bolt.NewBoltStore(&bolt.BoltStoreOptions{
-		Dbpath:  config.GetString("StorePath"),
-		Timeout: timeout,
-	})
-	if rs == nil {
-		logger.LogF("Cannot connect to file store", "db file path", rs.Dbpath)
-	} else {
-		logger.LogI("Connected to store backend", "driver", "boltdb", "db file path", rs.Dbpath)
-	}
-	r := render.New(render.Options{
-		Charset:    "UTF-8",
-		PrefixXML:  []byte("<?xml version='1.0' encoding='UTF-8'?>"),
-		IndentJSON: true,
-	})
-	apiCtx = &api.ApiContext{rs, r, securecookie.New(
-		securecookie.GenerateRandomKey(64),
-		securecookie.GenerateRandomKey(32))}
+
+	/*
+		apiCtx = &api.ApiContext{rs, r, securecookie.New(
+			securecookie.GenerateRandomKey(64),
+			securecookie.GenerateRandomKey(32))}
+	*/
 }
 
 func main() {
 	// Be sure we close the database when exit
-	defer apiCtx.DB.Close()
+	//	defer apiCtx.DB.Close()
 	setupSignals()
+	// Setup log
+	debug := false
+	if v, err := config.GetBool("Verbose"); err == nil && v {
+		logger.SetLevel(5) // logrus.DebugLevel
+		debug = true
+		logger.LogD("set log level to DebugLevel")
+	}
+
+	setupStoreManager()
+
+	// Setup api port
 	port := config.GetString("Port")
 	if port == "" {
 		logger.LogW("can't get Port value from config", "USING:", 8000)
@@ -102,48 +98,56 @@ func main() {
 	logger.LogI("Go", "Version", runtime.Version())
 	logger.LogI("API Server", "Status", "started", "port", port)
 
-	server := aloja.New().Port(port).SSLConf(config.GetString("SslCert"), config.GetString("SslKey"))
-	// Use CORS Handler in every request and log every request
+	server := echo.New()
+	server.SetDebug(debug)
+	server.Use(mw.Recover())
+	server.Use(mw.StripTrailingSlash())
+	server.Use(mw.Logger())
+	server.Get("/time", api.Time)
+	// serve the V1 REST API from /api/v1
+	apisrv := server.Group("/api/v1")
+	// Gzip
+	apisrv.Use(mw.Gzip())
+	// Use CORS Handler and log every request to the api
 	origins := strings.Split(config.GetString("Origins"), ",")
 	if len(origins) == 0 || origins[0] == "" {
 		origins = []string{"*"}
 	}
-	logger.LogI("main (cors)", "allowed origins", origins)
-	cors := mw.CorsHandler(mw.CorsOptions{
+	logger.LogD("setup cors", "allowed origins", origins)
+	apisrv.Use(cors.New(cors.Options{
 		AllowedOrigins:   origins,
 		AllowedMethods:   []string{"GET", "POST", "PUT", "PATCH", "OPTIONS", "DELETE"},
 		AllowCredentials: true,
 		Debug:            true,
-	})
-	server.AddGlobal(cors, mw.LogHandler)
+	}).Handler)
 
-	// serve the V1 REST API from /api/v1
-	apisrv := server.NewSubrouter("/api/v1")
 	setupAPIRoutes(apisrv)
-
-	// run the server
-	server.Run()
+	server.RunTLS(":"+port, config.GetString("SslCert"), config.GetString("SslKey"))
 }
 
 // setupAPIRoutes añade al router los puntos de acceso a los servicios ofrecidos
-func setupAPIRoutes(apisrv *aloja.Subrouter) {
+func setupAPIRoutes(apisrv *echo.Group) {
 	// accounts
-	apisrv.Get("/accounts", http.HandlerFunc(apiCtx.GetAllAccounts))
-	apisrv.Get("/accounts/:uid", http.HandlerFunc(apiCtx.GetAccountByID))
-	apisrv.Post("/accounts", http.HandlerFunc(apiCtx.NewAccount))
-	apisrv.Put("/accounts/:uid", http.HandlerFunc(apiCtx.UpdateAccount))
-	apisrv.Delete("/accounts/:uid", http.HandlerFunc(apiCtx.DeleteAccount))
+	apisrv.Get("/accounts", api.GetAllAccounts(mainManager))
+	apisrv.Get("/accounts/:uid", api.GetAccountByID(mainManager))
+	apisrv.Post("/accounts", api.NewAccount(mainManager))
+	apisrv.Put("/accounts/:uid", api.UpdateAccount(mainManager))
+	apisrv.Delete("/accounts/:uid", api.DeleteAccount(mainManager))
 	// Keys
-	apisrv.Get("/accounts/:uid/keys", http.HandlerFunc(apiCtx.GetAccountKeys))
+	apisrv.Get("/keys", api.GetAllKeys(mainManager))
+	apisrv.Get("/keys/:kid", api.GetKey(mainManager))
+	apisrv.Get("/accounts/:uid/keys", api.GetAccountKeys(mainManager))
+	apisrv.Delete("/keys/:kid", api.DeleteKey(mainManager))
 	// account jwt
-	apisrv.Get("/accounts/:uid/tokens", http.HandlerFunc(apiCtx.GetAccountTokens))
+	//apisrv.Get("/accounts/:uid/tokens", http.HandlerFunc(apiCtx.GetAccountTokens))
 
 	// authentication
-	apisrv.Post("/authenticate", http.HandlerFunc(apiCtx.Authenticate))
+	apisrv.Post("/authenticate", api.Authenticate(mainManager))
 
 	// JWT
 	apisrv.Post("/jwt/token/:uid", http.HandlerFunc(apiCtx.NewJWTToken))
 	apisrv.Post("/jwt/token/validate", http.HandlerFunc(apiCtx.ValidateToken))
+	apisrv.Get("/jwt/token/validate", http.HandlerFunc(apiCtx.ValidateToken))
 }
 
 // setupSignals configura la captura de señales de sistema y actúa basándose en ellas
@@ -152,8 +156,27 @@ func setupSignals() {
 	signal.Notify(sc, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	go func() {
 		for sig := range sc {
+			mainManager.Close()
 			logger.LogI("signal.notify", "captured signal", sig, "stopping", true)
 			os.Exit(1)
 		}
 	}()
+}
+
+func setupStoreManager() {
+	timeout := 5 * time.Second
+	if to, err := config.GetInt("StoreTimeout"); err == nil {
+		logger.LogI("Store", "connect timeout (s)", to)
+		timeout = time.Duration(to) * time.Second
+	}
+	var err error
+	mainManager, err = manager.NewManager(map[string]interface{}{"path": config.GetString("StorePath"), "timeout": timeout}, "boltdb")
+	if err != nil {
+		logger.LogF("Could not create Manager: %s", err)
+	}
+
+	err = mainManager.Init()
+	if err != nil {
+		logger.LogF("Could not Init manager", "pkg", "main", "func", "Init()", "error", err)
+	}
 }
